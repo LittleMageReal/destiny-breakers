97ff98773adcf440c383f3de04bbba459da658f5
LittleMageReal <breakersdestiny@gmail.com>
2024-06-15 10:19:32 +0300

    somethink like this

--- Assets/Resources/AIController.cs
+++ Assets/Resources/AIController.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
-   
+   public KArtController kartController; // Reference to the KArtController script
+
+    void Update()
+    {
+       kartController._forwardAmount = 1.0f; 
+       kartController._turnAmount = 1.0f;
+       kartController.Drive();
+       kartController.TurnHandler();
+    }
 }
